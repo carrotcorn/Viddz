223f2d1aaa390b8ef3709ca2ba677a6884ce17ba
carrotcorn <ericbourne6371@gmail.com>
2022-03-02 15:59:52 -0800

    started MVC project

---
+++ Viddz/Viddz/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Viddz.Models;
+
+namespace Viddz.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            Movie movie = new Movie("Shrek in Space!");
+            //var movie = new Movie() { Name = "Shrek in Space!" };
+
+            return View(movie);
+        }
+    }
+}
---
+++ Viddz/Viddz/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Viddz.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Movie() { }
+        public Movie(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
